603194f3578d558b51a12ef47f31ee90907cd46f
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-23 22:14:51 +0600

    Initial stage of UseReducer

--- src/context/ToDoProvider.tsx
+++ src/context/ToDoProvider.tsx
@@ -1,10 +1,28 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useReducer } from "react";
 
 type TTodoProviderProps = {
   children: ReactNode;
 };
+type TTodo = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+};
+
+const initialState: TTodo[] = [];
+
+const reducer = (currentState, action) => {
+  switch (action.type) {
+    case "addTodo":
+      return;
+
+    default:
+      return initialState;
+  }
+};
 export const ToDoContext = createContext(undefined);
 const ToDoProvider = ({ children }: TTodoProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const values = {
     todoTitle: "This is todo title",
   };
